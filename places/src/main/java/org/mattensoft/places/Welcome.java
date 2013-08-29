aca4e97cfce631a49ad78e459e11f6ff9f54f65a
georgenser <georgenser@mercator-mint>
2013-08-29 20:16:20 +0200

    fixed java ee 7 dependencies

--- places/src/main/java/org/mattensoft/places/Welcome.java
+++ places/src/main/java/org/mattensoft/places/Welcome.java
@@ -2,8 +2,10 @@ package org.mattensoft.places;
 
 import java.io.Serializable;
 import javax.faces.view.ViewScoped;
+import javax.inject.Named;
 
 @ViewScoped
+@Named
 public class Welcome implements Serializable{
     
     private String welcomeMessage = "Hello Places";
